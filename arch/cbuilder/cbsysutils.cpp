ec5b36b31acbbc011374943fd79cd5de24b8176e
KrasheninnikovMaxim <maxk008@gmail.com>
2009-09-08 13:28:37 +0000

    git-svn-id: https://5-10/svn/mxsrclib/trunk@38 63356120-0428-6b47-bcd6-cd59724df219

--- arch/cbuilder/cbsysutils.cpp
+++ arch/cbuilder/cbsysutils.cpp
@@ -99,7 +99,7 @@ void irs::cbuilder::file_xls_table_read(const string_t& a_book_name,
       for (size_type row_i = 0; row_i < row_count; row_i++) {
         Variant Cell = Cells.OlePropertyGet("Item", row_i+1, col_i+1);
         Variant CellValue = Cell.OlePropertyGet("Value");
-        AnsiString CellValueStr = CellValue;
+        AnsiString CellValueStr = static_cast<AnsiString>(CellValue);
           //static_cast<AnsiString>(CellValue.AsType(varString));
         ap_table_string->write_cell(col_i, row_i, CellValueStr.c_str());
       }
