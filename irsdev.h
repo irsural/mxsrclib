789ea9549cf52c24baaca272c5ac4afca0730310
SergeevSergey <sergeev@uralweb.ru>
2011-04-19 11:12:52 +0000

    git-svn-id: https://5-10/svn/mxsrclib/trunk@745 63356120-0428-6b47-bcd6-cd59724df219

--- irsdev.h
+++ irsdev.h
@@ -14,6 +14,10 @@
 #include <irscpu.h>
 #include <timer.h>
 
+#ifdef __ICCARM__
+#include <armioregs.h>
+#endif // __ICCARM__
+
 #define PWM_ZERO_PULSE 1
 
 #ifdef PWM_ZERO_PULSE
@@ -102,6 +106,32 @@ private:
     void interrupt();
   #endif  //  PWM_ZERO_PULSE
 };
+
+class wdt_t
+{
+public:
+  wdt_t(size_t a_period_s = 300):
+    m_period_s(a_period_s)
+  {
+    RCGC0_bit.WDT0 = 1;
+    for (irs_u8 i = 10; i; i--);
+    
+    WDT0LOCK = 0x1ACCE551;
+    WDT0LOAD = static_cast<irs_u32>(m_period_s*irs::cpu_traits_t::frequency());
+    WDT0CTL_bit.RESEN = 1;
+    WDT0CTL_bit.INTEN = 1;
+    WDT0TEST_bit.STALL = 1; // вероятно не работает с J-Link;
+    WDT0LOCK = 0x0;
+  }
+  void restart()
+  {
+    WDT0LOCK = 0x1ACCE551;
+    WDT0ICR = 0x11111111;
+    WDT0LOCK = 0x0;
+  }
+private:
+  size_t m_period_s;
+}; // wdt_t
 
 //! @}
 
