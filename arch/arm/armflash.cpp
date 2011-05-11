f535472b6d1a0b1b5a35fcae8a9ccff84086d7d1
SergeevSergey <sergeev@uralweb.ru>
2011-05-11 11:13:33 +0000

    Добавлен flash_protected_t
    git-svn-id: https://5-10/svn/mxsrclib/trunk@777 63356120-0428-6b47-bcd6-cd59724df219

--- arch/arm/armflash.cpp
+++ arch/arm/armflash.cpp
@@ -5,6 +5,7 @@
 
 #include <armflash.h>
 #include <mxdata.h>
+#include <irsalg.h>
 
 #include <irsfinal.h>
 
@@ -14,8 +15,21 @@ irs::arm::flash_t::flash_t(
 ):
   m_size(a_size),
   m_flash_end_addr(a_flash_end_addr),
-  m_flash_begin_addr(m_flash_end_addr - m_size)
+  m_flash_begin_addr(m_flash_end_addr - (m_size + m_crc_size)),
+  m_crc_error(false)
 {
+  irs_u32 crc_new = calc_new_crc();
+  irs_u32 crc32_flash = crc_flash();
+  if (crc_new == crc32_flash) {
+    m_crc_error = false;
+  } else {
+    irs_u32 crc_old = calc_old_crc();
+    if (crc_old == crc32_flash) {
+      m_crc_error = false;
+    } else {
+      m_crc_error = true;
+    }
+  }
 }
 
 irs::arm::flash_t::~flash_t()
@@ -35,31 +49,37 @@ irs_bool irs::arm::flash_t::connected()
 
 void irs::arm::flash_t::read(irs_u8 *ap_buf, irs_uarc a_index, irs_uarc a_size)
 {
-  IRS_LIB_ASSERT((m_flash_begin_addr + a_index + a_size) <= m_flash_end_addr);
-  irs_u32 index = m_flash_begin_addr + a_index;
+  /*IRS_LIB_ASSERT((m_flash_begin_addr + m_crc_size + a_index + a_size) <=
+    m_flash_end_addr);*/
+  irs_u32 index = m_flash_begin_addr + m_crc_size + a_index;
   memcpyex(ap_buf, reinterpret_cast<irs_u8*>(index), a_size);
 }
 
 void irs::arm::flash_t::write(const irs_u8 *ap_buf, irs_uarc a_index,
   irs_uarc a_size)
 {
-  IRS_LIB_ASSERT((m_flash_begin_addr + a_index + a_size) <= m_flash_end_addr);
+  /*IRS_LIB_ASSERT((m_flash_begin_addr + m_crc_size + a_index + a_size) <=
+    m_flash_end_addr);*/
   raw_data_t<irs_u8> p_data(m_flash_page_size);
   irs_u32 index = m_flash_begin_addr + a_index -
     ((m_flash_begin_addr + a_index)%m_flash_page_size);
   flash_read_block(p_data.data(), index);
-  memcpyex(p_data.data() + ((m_flash_begin_addr + a_index)%m_flash_page_size),
-    ap_buf, a_size);
+  memcpyex(p_data.data() + ((m_flash_begin_addr + m_crc_size + a_index)%
+    m_flash_page_size), ap_buf, a_size);
+  irs_u32 crc = crc32_table(p_data.data() + ((m_flash_begin_addr + m_crc_size)%
+    m_flash_page_size), m_size);
+  memcpyex(p_data.data() + m_flash_begin_addr%m_flash_page_size,
+    reinterpret_cast<irs_u8*>(&crc), m_crc_size);
   flash_erase(index);
   flash_write_block(p_data.data(), index);
 }
 
 irs_bool irs::arm::flash_t::bit(irs_uarc a_index, irs_uarc a_bit_index)
 {
-  IRS_LIB_ASSERT((m_flash_begin_addr + a_index + 1) <= m_flash_end_addr);
+  //IRS_LIB_ASSERT((m_flash_begin_addr + a_index + 1) <= m_flash_end_addr);
   if (a_index >= m_size) return false;
   if (a_bit_index > 7) return false;
-  irs_u32 index = m_flash_begin_addr + a_index;
+  irs_u32 index = m_flash_begin_addr + m_crc_size + a_index;
   irs_u8 byte = 0;
   memcpyex(&byte, reinterpret_cast<irs_u8*>(index), 1);
   return (byte&static_cast<irs_u8>(1 << a_bit_index));
@@ -67,37 +87,45 @@ irs_bool irs::arm::flash_t::bit(irs_uarc a_index, irs_uarc a_bit_index)
 
 void irs::arm::flash_t::set_bit(irs_uarc a_index, irs_uarc a_bit_index)
 {
-  IRS_LIB_ASSERT((m_flash_begin_addr + a_index + 1) <= m_flash_end_addr);
+  //IRS_LIB_ASSERT((m_flash_begin_addr + a_index + 1) <= m_flash_end_addr);
   if (a_index >= m_size) return;
   if (a_bit_index > 7) return;
   raw_data_t<irs_u8> p_data(m_flash_page_size);
   irs_u32 index = m_flash_begin_addr + a_index -
     ((m_flash_begin_addr + a_index)%m_flash_page_size);
   flash_read_block(p_data.data(), index);
-  p_data[(m_flash_begin_addr + a_index)%m_flash_page_size] |=
+  p_data[(m_flash_begin_addr + m_crc_size + a_index)%m_flash_page_size] |=
     static_cast<irs_u8>(1 << a_bit_index);
+  irs_u32 crc = crc32_table(p_data.data() + ((m_flash_begin_addr + m_crc_size)%
+    m_flash_page_size), m_size);
+  memcpyex(p_data.data() + m_flash_begin_addr%m_flash_page_size,
+    reinterpret_cast<irs_u8*>(&crc), m_crc_size);
   flash_erase(index);
   flash_write_block(p_data.data(), index);
 }
 
 void irs::arm::flash_t::clear_bit(irs_uarc a_index, irs_uarc a_bit_index)
 {
-  IRS_LIB_ASSERT((m_flash_begin_addr + a_index + 1) <= m_flash_end_addr);
+  //IRS_LIB_ASSERT((m_flash_begin_addr + a_index + 1) <= m_flash_end_addr);
   if (a_index >= m_size) return;
   if (a_bit_index > 7) return;
   raw_data_t<irs_u8> p_data(m_flash_page_size);
   irs_u32 index = m_flash_begin_addr + a_index -
     ((m_flash_begin_addr + a_index)%m_flash_page_size);
   flash_read_block(p_data.data(), index);
-  p_data[(m_flash_begin_addr + a_index)%m_flash_page_size] &=
+  p_data[(m_flash_begin_addr + m_crc_size + a_index)%m_flash_page_size] &=
     static_cast<irs_u8>((1 << a_bit_index)^0xFF);
+  irs_u32 crc = crc32_table(p_data.data() + ((m_flash_begin_addr + m_crc_size)%
+    m_flash_page_size), m_size);
+  memcpyex(p_data.data() + m_flash_begin_addr%m_flash_page_size,
+    reinterpret_cast<irs_u8*>(&crc), m_crc_size);
   flash_erase(index);
   flash_write_block(p_data.data(), index);
 }
 
 void irs::arm::flash_t::flash_erase(irs_u32 a_index)
 {
-  IRS_LIB_ASSERT((a_index + m_flash_page_size) <= m_flash_end_addr);
+  //IRS_LIB_ASSERT((a_index + m_flash_page_size) <= m_flash_end_addr);
   FCMISC_bit.AMISC = 1;
   FMA = a_index;
   FMC = m_WRITE_KEY | m_ERASE;
@@ -110,7 +138,7 @@ void irs::arm::flash_t::flash_erase(irs_u32 a_index)
 void irs::arm::flash_t::flash_write_block(irs_u8* ap_buf,
   irs_u32 a_index)
 {
-  IRS_LIB_ASSERT((a_index + m_flash_page_size) <= m_flash_end_addr);
+  //IRS_LIB_ASSERT((a_index + m_flash_page_size) <= m_flash_end_addr);
   irs_u32 size = m_flash_page_size;
   FCMISC_bit.AMISC = 1;
   while(size)
@@ -131,11 +159,121 @@ void irs::arm::flash_t::flash_write_block(irs_u8* ap_buf,
 
 void irs::arm::flash_t::flash_read_block(irs_u8* ap_buf, irs_u32 a_index)
 {
-  IRS_LIB_ASSERT((a_index + m_flash_page_size) <= m_flash_end_addr);
+  //IRS_LIB_ASSERT((a_index + m_flash_page_size) <= m_flash_end_addr);
   memcpyex(ap_buf, reinterpret_cast<irs_u8*>(a_index),
     m_flash_page_size);
 }
 
+irs_u32 irs::arm::flash_t::crc_flash()
+{
+  irs_u32 crc = 0;
+  memcpyex(reinterpret_cast<irs_u8*>(&crc),
+    reinterpret_cast<irs_u8*>(m_flash_begin_addr), m_crc_size);
+  return crc;
+}
+
+irs_u32 irs::arm::flash_t::calc_old_crc() const
+{
+  return crc32(reinterpret_cast<irs_u32*>(m_flash_begin_addr + m_crc_size),
+    0, m_size/4);
+}
+
+irs_u32 irs::arm::flash_t::calc_new_crc() const
+{
+  return crc32_table(reinterpret_cast<irs_u8*>(m_flash_begin_addr + m_crc_size),
+    m_size);
+}
+
 void irs::arm::flash_t::tick()
 {
+}
+
+bool irs::arm::flash_t::error()
+{
+  return m_crc_error;
+}
+
+//------------------------------------------------------------------------------
+
+irs::arm::flash_protected_t::flash_protected_t(
+  size_t a_size,
+  size_t a_flash_end_addr
+):
+  m_size(a_size),
+  m_flash1(a_size, a_flash_end_addr),
+  m_flash2(a_size, a_flash_end_addr - m_flash_page_size),
+  m_crc_error(false)
+{
+  if (m_flash1.error() && !m_flash2.error())
+  {
+    irs_u8 *temp = new irs_u8[m_size];
+    m_flash2.read(temp, 0, m_size);
+    m_flash1.write(temp, 0, m_size);
+    delete []temp;
+  }
+  else if (m_flash1.error() && m_flash2.error())
+  {
+    irs_u8 *temp = new irs_u8[m_size];
+    m_flash1.read(temp, 0, m_size);
+    m_flash2.write(temp, 0, m_size);
+    delete []temp;
+  }
+  else if (m_flash1.error() && m_flash2.error()) m_crc_error = true;
+}
+
+irs::arm::flash_protected_t::~flash_protected_t()
+{
+}
+
+irs_uarc irs::arm::flash_protected_t::size()
+{
+  return m_size;
+}
+
+irs_bool irs::arm::flash_protected_t::connected()
+{
+  return true;
+}
+
+void irs::arm::flash_protected_t::read(irs_u8 *ap_buf, irs_uarc a_index,
+  irs_uarc a_size)
+{
+  m_flash1.read(ap_buf, a_index, a_size);
+}
+
+void irs::arm::flash_protected_t::write(const irs_u8 *ap_buf, irs_uarc a_index,
+  irs_uarc a_size)
+{
+  m_flash1.write(ap_buf, a_index, a_size);
+  m_flash2.write(ap_buf, a_index, a_size);
+}
+
+irs_bool irs::arm::flash_protected_t::bit(irs_uarc a_index,
+  irs_uarc a_bit_index)
+{
+  return m_flash1.bit(a_index, a_bit_index);
+}
+
+void irs::arm::flash_protected_t::set_bit(irs_uarc a_index,
+  irs_uarc a_bit_index)
+{
+  m_flash1.set_bit(a_index, a_bit_index);
+  m_flash2.set_bit(a_index, a_bit_index);
+}
+
+void irs::arm::flash_protected_t::clear_bit(irs_uarc a_index,
+  irs_uarc a_bit_index)
+{
+  m_flash1.clear_bit(a_index, a_bit_index);
+  m_flash2.clear_bit(a_index, a_bit_index);
+}
+
+void irs::arm::flash_protected_t::tick()
+{
+
+}
+
+bool irs::arm::flash_protected_t::double_error()
+{
+  return m_crc_error;
 }
--- arch/arm/armflash.h
+++ arch/arm/armflash.h
@@ -30,11 +30,13 @@ public:
   virtual void set_bit(irs_uarc a_index, irs_uarc a_bit_index);
   virtual void clear_bit(irs_uarc a_index, irs_uarc a_bit_index);
   virtual void tick();
+  bool error();
 private:
   enum {
     m_flash_page_size = 0x400, // 1 kB
     m_irs_u32_size = sizeof(irs_u32),
-    m_buf_size = m_flash_page_size
+    m_buf_size = m_flash_page_size,
+    m_crc_size = sizeof(irs_u32)
   };
   enum {
     m_WRITE_KEY = 0xA4420000,
@@ -45,11 +47,42 @@ private:
   size_type m_size;
   size_type m_flash_end_addr;
   size_type m_flash_begin_addr;
+  bool m_crc_error;
   
   void flash_erase(irs_u32 a_index);
   void flash_write_block(irs_u8* ap_buf, irs_u32 a_index);
   void flash_read_block(irs_u8* ap_buf, irs_u32 a_index);
+  irs_u32 crc_flash();
+  irs_u32 calc_old_crc() const;
+  irs_u32 calc_new_crc() const;
 }; // flash_t
+
+class flash_protected_t: public mxdata_t
+{
+public:
+  typedef flash_t::size_type size_type;
+  
+  flash_protected_t(size_t a_size, size_t a_flash_end_addr = 0x40000);
+  virtual ~flash_protected_t();
+  virtual irs_uarc size();
+  virtual irs_bool connected();
+  virtual void read(irs_u8 *ap_buf, irs_uarc a_index, irs_uarc a_size);
+  virtual void write(const irs_u8 *ap_buf, irs_uarc a_index, irs_uarc a_size);
+  virtual irs_bool bit(irs_uarc a_index, irs_uarc a_bit_index);
+  virtual void set_bit(irs_uarc a_index, irs_uarc a_bit_index);
+  virtual void clear_bit(irs_uarc a_index, irs_uarc a_bit_index);
+  virtual void tick();
+  bool double_error();
+private:
+  enum {
+    m_crc_size = sizeof(irs_u32),
+    m_flash_page_size = 0x400 // 1 kB
+  };
+  size_type m_size;
+  flash_t m_flash1;
+  flash_t m_flash2;
+  bool m_crc_error;
+}; // flash_protected_t
 
 } // namespace arm
 
