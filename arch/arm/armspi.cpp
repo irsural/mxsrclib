266d3e9f91941cbdd96629a5a2506d8ccd1c2d96
u309m_user <elec@irsural.ru>
2012-04-13 11:44:24 +0000

    git-svn-id: https://5-10/svn/mxsrclib/trunk@912 63356120-0428-6b47-bcd6-cd59724df219

--- arch/arm/armspi.cpp
+++ arch/arm/armspi.cpp
@@ -389,6 +389,11 @@ void irs::arm::arm_spi_t::write(const irs_u8* ap_buf, irs_uarc a_size)
     mp_write_buf = ap_buf;
     m_cur_byte = 0;
     m_status = SPI_WRITE;
+    if (m_ssi_type == SSI1) {
+      if ((mp_write_buf[1] & (1 << 2)) != 0) {
+        irs::mlog() << "бит on" << endl;
+      }
+    }
   }
 }
 
@@ -561,7 +566,7 @@ void irs::arm::arm_spi_t::read_write(irs_u8 *ap_read_buf,
     m_cur_byte = 0;
     m_status = SPI_RW_WRITE;
     if (m_ssi_type == SSI1) {
-      if ((mp_write_buf[0] & (1 << 2)) != 0) {
+      if ((mp_write_buf[1] & (1 << 2)) != 0) {
         irs::mlog() << "бит on" << endl;
       }
     }
