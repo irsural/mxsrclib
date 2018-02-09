6ff859618e6b9a10a8efe51b0e2a3d6063e7dfd3
KrasheninnikovMaxim <maxk008@gmail.com>
2018-02-09 18:27:51 +0500

    Исправлена коррекция в классе correct_t
    То что было раньше приводило к квадратичной зависимости между точками, а не к линейной
    Программа компилировалась, но не прошивалась

--- correct_alg.h
+++ correct_alg.h
@@ -310,10 +310,6 @@ template <class IN_X, class IN_Y, class OUT_Z, class K>
 class correct_t
 {
 public:
-  correct_t(mxdata_t *ap_data, irs_uarc a_start_index, bool a_use_b);
-  ~correct_t();
-  OUT_Z apply(IN_X a_x_value, IN_Y a_y_value, OUT_Z a_in_value);
-private:
   struct triple_line_data_t
   {
     K x1;
@@ -326,8 +322,20 @@ private:
     K z22;
     K x0;
     K y0;
+    irs_uarc x_offset;
+    irs_uarc x_index;
+    irs_uarc y_index;
   };
 
+  correct_t(mxdata_t *ap_data, irs_uarc a_start_index, bool a_use_b);
+  ~correct_t();
+  void preapre_data(IN_X a_x_value, IN_Y a_y_value, OUT_Z a_in_value,
+    triple_line_data_t* ap_tl_data);
+  OUT_Z apply(IN_X a_x_value, IN_Y a_y_value, OUT_Z a_in_value);
+  OUT_Z apply_dac(IN_X a_x_value, IN_Y a_y_value);
+  K triple_line(triple_line_data_t a_data);
+  irs::correct_map_t<IN_X, IN_Y, OUT_Z, K>* map();
+private:
   irs_uarc m_start_index;
   mxdata_t *mp_data;
   irs::correct_map_t<IN_X, IN_Y, OUT_Z, K> m_map;
@@ -335,7 +343,6 @@ private:
   irs_bool m_error;
   bool m_use_b;
 
-  K triple_line(triple_line_data_t a_data);
   irs_bool test_map();
 };
 
@@ -359,114 +366,127 @@ irs::correct_t<IN_X, IN_Y, OUT_Z, K>::~correct_t()
 }
 
 template <class IN_X, class IN_Y, class OUT_Z, class K>
-OUT_Z irs::correct_t<IN_X, IN_Y, OUT_Z, K>::apply(IN_X a_x_value,
-  IN_Y a_y_value, OUT_Z a_in_value)
+irs::correct_map_t<IN_X, IN_Y, OUT_Z, K>*
+irs::correct_t<IN_X, IN_Y, OUT_Z, K>::
+map()
 {
-  OUT_Z out_value = a_in_value;
+  return mp_map;
+}
+
+template <class IN_X, class IN_Y, class OUT_Z, class K>
+void irs::correct_t<IN_X, IN_Y, OUT_Z, K>::
+preapre_data(IN_X a_x_value, IN_Y a_y_value, OUT_Z a_in_value,
+  triple_line_data_t* ap_tl_data)
+{
+  memsetex(ap_tl_data, sizeof(*ap_tl_data));
   if (mp_data->connected())
   {
+    triple_line_data_t& tl_data = *ap_tl_data;
     mp_map->connect(mp_data, m_start_index, m_use_b);
-    if (m_error)
-    {
+    if (m_error) {
       if (test_map()) m_error = irs_false;
     }
-    if (!m_error)
-    {
+    if (!m_error) {
       irs_uarc x_offset = 0;
       irs_uarc x_index = 0;
       irs_uarc y_index = 0;
-      triple_line_data_t m_data;
 
       if (a_y_value >= mp_map->mp_y_points[mp_map->m_y_count-1])
       {
         y_index = mp_map->m_y_count - 1;
-        x_offset = y_index * mp_map->m_x_count;
       }
       else
       {
         for (y_index = 0; y_index < mp_map->m_y_count; y_index++)
-          if (a_y_value < mp_map->mp_y_points[y_index])
-          {
-            x_offset = y_index * mp_map->m_x_count;
+          if (a_y_value < mp_map->mp_y_points[y_index]) {
+            if (y_index < 1) {
+              y_index = 1;
+            }
             break;
           }
       }
-      if (y_index < 1)
-      {
-        y_index = 1;
-        x_offset = mp_map->m_x_count;
-      }
-      if (a_x_value >= mp_map->mp_x_points[mp_map->m_x_count-1])
-      {
+      x_offset = y_index * mp_map->m_x_count;
+      if (a_x_value >= mp_map->mp_x_points[mp_map->m_x_count-1]) {
         x_index = mp_map->m_x_count - 1;
-
-        m_data.x1 = mp_map->mp_x_points[x_index - 1];
-        m_data.x2 = mp_map->mp_x_points[x_index];
-        m_data.y1 = mp_map->mp_y_points[y_index - 1];
-        m_data.y2 = mp_map->mp_y_points[y_index];
-        m_data.z11 = mp_map->mp_k_array[(x_index - 1)
-          + (x_offset - mp_map->m_x_count)];
-        m_data.z21 = mp_map->mp_k_array[x_index
-          + (x_offset - mp_map->m_x_count)];
-        m_data.z12 = mp_map->mp_k_array[(x_index - 1)+ x_offset];
-        m_data.z22 = mp_map->mp_k_array[x_index + x_offset];
-        m_data.x0 = a_x_value;
-        m_data.y0 = a_y_value;
-
-        K k = triple_line(m_data);
-
-        if (m_use_b)
-        {
-          m_data.z11 = mp_map->mp_b_array[(x_index - 1)
-            + (x_offset - mp_map->m_x_count)];
-          m_data.z21 = mp_map->mp_b_array[x_index
-            + (x_offset - mp_map->m_x_count)];
-          m_data.z12 = mp_map->mp_b_array[(x_index - 1)+ x_offset];
-          m_data.z22 = mp_map->mp_b_array[x_index + x_offset];
-
-          K b = triple_line(m_data);
-
-          out_value = OUT_Z(K(a_in_value) * k + b);
-        }
-        else out_value = OUT_Z(K(a_in_value) * k);
-      }
-      else
-      {
-        for (x_index = 0; x_index < mp_map->m_x_count; x_index++)
+      } else {
+        for (x_index = 0; x_index < mp_map->m_x_count; x_index++) {
           if (a_x_value < mp_map->mp_x_points[x_index])
           {
-            if (x_index < 1) x_index = 1;
-            m_data.x1 = mp_map->mp_x_points[x_index - 1];
-            m_data.x2 = mp_map->mp_x_points[x_index];
-            m_data.y1 = mp_map->mp_y_points[y_index - 1];
-            m_data.y2 = mp_map->mp_y_points[y_index];
-            m_data.z11 = mp_map->mp_k_array[(x_index - 1)
-              + (x_offset - mp_map->m_x_count)];
-            m_data.z21 = mp_map->mp_k_array[x_index
-              + (x_offset - mp_map->m_x_count)];
-            m_data.z12 = mp_map->mp_k_array[(x_index - 1)+ x_offset];
-            m_data.z22 = mp_map->mp_k_array[x_index + x_offset];
-            m_data.x0 = a_x_value;
-            m_data.y0 = a_y_value;
-
-            K k = triple_line(m_data);
-
-            if (m_use_b)
-            {
-              m_data.z11 = mp_map->mp_b_array[(x_index - 1)
-                + (x_offset - mp_map->m_x_count)];
-              m_data.z21 = mp_map->mp_b_array[x_index
-                + (x_offset - mp_map->m_x_count)];
-              m_data.z12 = mp_map->mp_b_array[(x_index - 1)+ x_offset];
-              m_data.z22 = mp_map->mp_b_array[x_index + x_offset];
-
-              K b = triple_line(m_data);
-
-              out_value = OUT_Z(K(a_in_value) * k + b);
+            if (x_index < 1) {
+              x_index = 1;
             }
-            else out_value = OUT_Z(K(a_in_value) * k);
             break;
           }
+        }
+      }
+      tl_data.x1 = mp_map->mp_x_points[x_index - 1];
+      tl_data.x2 = mp_map->mp_x_points[x_index];
+      tl_data.y1 = mp_map->mp_y_points[y_index - 1];
+      tl_data.y2 = mp_map->mp_y_points[y_index];
+      tl_data.x0 = a_x_value;
+      tl_data.y0 = a_y_value;
+      tl_data.x_offset = x_offset;
+      tl_data.x_index = x_index;
+      tl_data.y_index = y_index;
+    }
+  }
+}
+
+template <class IN_X, class IN_Y, class OUT_Z, class K>
+OUT_Z irs::correct_t<IN_X, IN_Y, OUT_Z, K>::
+apply(IN_X a_x_value, IN_Y a_y_value, OUT_Z a_in_value)
+{
+  OUT_Z out_value = a_in_value;
+
+  if (mp_data->connected())
+  {
+    triple_line_data_t tl_data;
+    preapre_data(a_x_value, a_y_value, a_in_value, &tl_data);
+
+    if (!m_error)
+    {
+      irs_uarc x_offset = tl_data.x_offset;
+      irs_uarc x_index = tl_data.x_index;
+      irs_uarc y_index = tl_data.y_index;
+
+      if (m_use_b) {
+        // Старый алгоритм, который используется только в фазе
+        tl_data.z11 = mp_map->mp_k_array[(x_index - 1)
+          + (x_offset - mp_map->m_x_count)];
+        tl_data.z21 = mp_map->mp_k_array[x_index
+          + (x_offset - mp_map->m_x_count)];
+        tl_data.z12 = mp_map->mp_k_array[(x_index - 1) + x_offset];
+        tl_data.z22 = mp_map->mp_k_array[x_index + x_offset];
+
+        K k = triple_line(tl_data);
+
+        tl_data.z11 = mp_map->mp_b_array[(x_index - 1)
+          + (x_offset - mp_map->m_x_count)];
+        tl_data.z21 = mp_map->mp_b_array[x_index
+          + (x_offset - mp_map->m_x_count)];
+        tl_data.z12 = mp_map->mp_b_array[(x_index - 1) + x_offset];
+        tl_data.z22 = mp_map->mp_b_array[x_index + x_offset];
+
+        K b = triple_line(tl_data);
+
+        K in = static_cast<K>(a_in_value);
+
+        K out = k*in + b;
+
+        out_value = static_cast<OUT_Z>(out);
+      } else {
+        // Новый алгоритм, который используется для АЦП (ток и напряжение)
+        // Должно быть так: a_y_value == a_in_value
+        IN_Y y1 = mp_map->mp_y_points[y_index - 1];
+        IN_Y y = mp_map->mp_y_points[y_index];
+        tl_data.z11 = y1*mp_map->mp_k_array[(x_index - 1)
+          + (x_offset - mp_map->m_x_count)];
+        tl_data.z21 = y1*mp_map->mp_k_array[x_index
+          + (x_offset - mp_map->m_x_count)];
+        tl_data.z12 = y*mp_map->mp_k_array[(x_index - 1) + x_offset];
+        tl_data.z22 = y*mp_map->mp_k_array[x_index + x_offset];
+
+        out_value = triple_line(tl_data);
       }
     }
   }
@@ -474,8 +494,47 @@ OUT_Z irs::correct_t<IN_X, IN_Y, OUT_Z, K>::apply(IN_X a_x_value,
 }
 
 template <class IN_X, class IN_Y, class OUT_Z, class K>
-K irs::correct_t<IN_X, IN_Y, OUT_Z, K>::triple_line(
-  triple_line_data_t a_data)
+OUT_Z irs::correct_t<IN_X, IN_Y, OUT_Z, K>::
+apply_dac(IN_X a_x_value, IN_Y a_y_value)
+{
+  // a_in_value убрано, так как a_y_value == a_in_value
+  IRS_LIB_DBG_MSG_IF(m_use_b, "Функцию OUT_Z irs::correct_t<IN_X, IN_Y, "
+    "OUT_Z, K>::apply_dac нельзя вызывать когда включен параметр m_use_b");
+
+  OUT_Z out_value = static_cast<OUT_Z>(a_y_value);
+
+  if (mp_data->connected())
+  {
+    triple_line_data_t tl_data;
+    preapre_data(a_x_value, a_y_value, a_y_value, &tl_data);
+
+    if (!m_error)
+    {
+      irs_uarc x_offset = tl_data.x_offset;
+      irs_uarc x_index = tl_data.x_index;
+      irs_uarc y_index = tl_data.y_index;
+
+      if (!m_use_b) {
+        // Новый алгоритм, который используется для ЦАП (ток и напряжение)
+        IN_Y y1 = mp_map->mp_y_points[y_index - 1];
+        IN_Y y = mp_map->mp_y_points[y_index];
+        tl_data.z11 = y1/mp_map->mp_k_array[(x_index - 1)
+          + (x_offset - mp_map->m_x_count)];
+        tl_data.z21 = y1/mp_map->mp_k_array[x_index
+          + (x_offset - mp_map->m_x_count)];
+        tl_data.z12 = y/mp_map->mp_k_array[(x_index - 1) + x_offset];
+        tl_data.z22 = y/mp_map->mp_k_array[x_index + x_offset];
+
+        out_value = triple_line(tl_data);
+      }
+    }
+  }
+  return out_value;
+}
+
+template <class IN_X, class IN_Y, class OUT_Z, class K>
+K irs::correct_t<IN_X, IN_Y, OUT_Z, K>::
+triple_line(triple_line_data_t a_data)
 {
   K k, b, z01, z02;
   K delta_x = a_data.x2 - a_data.x1;
