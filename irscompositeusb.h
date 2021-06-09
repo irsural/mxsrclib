7da78000c375e6714472c33e565f6bad98b7f2bc
qucals <totalboy@outlook.com>
2021-06-09 17:38:02 +0500

    Переписываю логику usb_composite.

--- irscompositeusb.h
+++ irscompositeusb.h
@@ -1,6 +1,6 @@
 // @brief описание классов для работы с композиционным usb.
 //
-// Дата: 28.05.2021
+// Дата: 09.06.2021
 // Дата создания: 23.05.2021
 
 #pragma once
@@ -12,6 +12,7 @@
 #include <irscpp.h>
 #include <irsnetdefs.h>
 #include <irserror.h>
+#include <irsusbgeneric.h>
 
 #include <stm32h7xx_hal.h>
 #include <stm32h7xx_hal_conf.h>
@@ -20,168 +21,53 @@
 
 namespace irs
 {
-  
-typedef USBD_StatusTypeDef usb_statusns_t;
-  
-enum USB_ENDPOINT_TYPES {
-  USB_GENERIC_ENDPOINT_TYPE_BULK = 0,
-  USB_GENERIC_ENDPOINT_TYPE_CONTROL,
-  USB_GENERIC_ENDPOINT_TYPE_ISO,
-  USB_GENERIC_ENDPOINT_TYPE_INTERRUPT
-};
-  
-typedef struct usb_endpoint
-{
-  void (*callback)(void);
-  void* pma;
-  irs_u16 pma_size;
-  irs_u8 address;
-  irs_u8 type : 2;
-  irs_u8 tx : 1; // 1 - TX, 0 - RX
-  irs_u8 exclusive : 1; // 1 - если невозможно использовать точку для tx и rx
-  irs_u8 align : 1; // если следующая конечная точка противоположного 
-                    // типа имеет тот же номер конечной точки, что и эта
-} usb_endpoint;
-  
-typedef struct usb_module
-{
-  irs_u8 count_interfaces;
-  irs_u8 count_endpoints;
-  
-  irs_u8 start_inferface;
-  irs_u16 descriptor_size;
-  
-  void (*get_descriptor)(irs_u8* out);
-  void (*module_init)(void);
-  void (*module_reset)(void);
-  void (*module_set_configuration)(void);
-  void (*module_clear_feature)(void);
-  void (*clear)(void);
-  usb_statusns_t (*module_data_setup)(irs_u8 request, irs_u8 interface, 
-                                      irs_u8 request_type, irs_u8 w_value_0,
-                                      irs_u8 w_value_1, irs_u16 w_index,
-                                      irs_u16 w_length);
-  usb_statusns_t (*module_no_data_setup)(irs_u8 request, irs_u8 interface, 
-                                         irs_u8 request_type, irs_u8 w_value_0,
-                                         irs_u8 w_value_1, irs_u16 w_index,
-                                         irs_u16 w_length);
-  list<usb_endpoint*> endpoints_list;
-} usb_module;
 
-class composite_usb
-{
-public:
-  composite_usb(irs_u16 a_vendor_id = 0,
-                irs_u16 a_product_id = 0,
-                const char* ap_manufacturer = nullptr,
-                const char* ap_product = nullptr,
-                const char* ap_serial_number = nullptr,
-                list<usb_module*> a_modules_list = list<usb_module*>());
-  
-  virtual ~composite_usb();
-  
-  virtual void tick();
-  
-  void set_vendor_id(irs_u16 a_vendor_id);
-  
-  void set_product_id(irs_u16 a_product_id);
-  
-  void set_manufacturer(const char* ap_manufacturer);
-  
-  void set_product(const char* ap_product);
-  
-  void set_serial_number(const char* ap_serial_number);
-  
-  size_t count_modules() const;
-  
-  bool is_ready() const;
-  
-  void reset_modules();
-  
-  bool add_module(usb_module* module);
-  
-private:
-  /*Приватные функции*/
-  
-  int usb_init();
-  
-  /* Приватные поля */
-  
-  irs_u16 m_vendor_id;
-  irs_u16 m_product_id;
-  
-  const char* mp_manufacturer;
-  const char* mp_product;
-  const char* mp_serial_number;
-  
-  bool m_is_enabled;
-  
-  list<usb_module*> m_modules_list;
-  
-  USBD_HandleTypeDef m_handle_device;
-};
+#define MAX_USB_MODULES 6
 
-composite_usb::composite_usb(irs_u16 a_vendor_id,
-                             irs_u16 a_product_id,
-                             const char* ap_manufacturer,
-                             const char* ap_product,
-                             const char* ap_serial_number,
-                             list<usb_module*> a_modules_list)
-  : m_vendor_id(a_vendor_id)
-  , m_product_id(a_product_id)
-  , mp_manufacturer(ap_manufacturer)
-  , mp_product(ap_product)
-  , mp_serial_number(ap_serial_number)
-  , m_modules_list(a_modules_list)
-{ IRS_ASSERT(usb_init() < 0); }
-
-composite_usb::~composite_usb()
+class usb_module
 {}
 
-void composite_usb::tick()
+class usb_composite_device
 {
-}
+  usb_composite_device(
+    string ap_manufacturer,
+    string ap_product,
+    string ap_serial_number,
+    isr_u16 a_vendor_id,
+    irs_u16 a_product_id);
+  ~usb_composite_device();
 
-int composite_usb::usb_init()
-{
-  // Нужно ли инициализировать HAL здесь?
-  // HAL_PWREx_EnableUSBVoltageDetector();
-  
+  string get_manufacturer() const;
+  string get_product() const;
+  string get_serial_number() const;
 
-  USBD_Start(&m_handle_device);
-  
-  return 0;
-}
+  irs_u16 get_vendor_id() const;
+  irs_u16 get_product_id() const;
 
-void composite_usb::set_vendor_id(irs_u16 a_vendor_id)
-{ m_vendor_id = a_vendor_id; }
+  bool start();
+  void stop();
 
-void composite_usb::set_product_id(irs_u16 a_product_id)
-{ m_product_id = a_product_id; }
+  // Я не знаю, для чего эта функция
+  void clear();
 
-void composite_usb::set_manufacturer(const char* ap_manufacturer)
-{ mp_manufacturer = ap_manufacturer; }
+  // В миллисекундах
+  void set_disconnect_delay(irs_u32 delay = 500);
 
-void composite_usb::set_product(const char* ap_product)
-{ mp_product = ap_product; }
+  bool is_ready() const;
 
-void composite_usb::set_serial_number(cosnt char* ap_serial_number)
-{ mp_serial_number = ap_serial_number; }
+  bool add_module(usb_module* module);
 
-size_t composite_usb::count_modules() const
-{ return m_modules_list.size(); }
+private:
+  string mp_manufacturer;
+  string mp_product;
+  string mp_serial_number;
 
-bool composite_usb::is_ready() const
-{ return is_enabled; }
+  irs_u16 m_vendor_id;
+  irs_u16 m_product_id;
 
-void composite_usb::reset_modules()
-{ m_modules_list.clear(); }
+  bool m_is_enabled;
 
-bool composite_usb::add_module(usb_module* module)
-{
-  IRS_ASSERT(module == nullptr);
-  
-  return true;
+  list<usb_module*> m_modules_list;
 }
 
 } // namespace irs
