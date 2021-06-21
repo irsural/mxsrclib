2039ba33920d3b1cb139615e66a69c9bf73da5fd
qucals <totalboy@outlook.com>
2021-06-21 15:05:56 +0500

    Описаны необходимые дескрипторы для работы с USB в качестве структур.

--- irscompositeusb.h
+++ irscompositeusb.h
@@ -1,6 +1,6 @@
 // @brief описание классов для работы с композиционным usb.
 //
-// Дата: 20.06.2021
+// Дата: 21.06.2021
 // Дата создания: 23.05.2021
 
 #pragma once
@@ -155,88 +155,274 @@ namespace irs
  *   - 0xc0 - end_collection
  */
 
- /* TODO: Упростить работу с парами переменных старший/младший байты
-  *       посредству написания приватной функции деления числа на старшую часть и младшую
-  */
+/* TODO: Упростить работу с парами переменных старший/младший байты
+*       посредству написания приватной функции деления числа на старшую часть и младшую
+*/
 
-typedef struct
+struct basic_desc
 {
   irs_u8 length; // общая длина дескриптора
   irs_u8 desc_type; // показывает, что за дескриптор – определяет его тип
-} basic_desc;
+};
 
-typedef struct : public basic_desc
+struct desc_string_handshake : public basic_desc
 {
-  irs_u8 number; // порядковый номер интерфейса
-  irs_u8 alternate_setting; // признак альтернативного интерфейса
-  irs_u8 count_endpoints; // количество конечных точек
-  irs_u8 iclass; // класс интерфейса (например, HID)
-  irs_u8 subclass; // сабкласс интерфейса
-  irs_u8 protocol; // протокол интерфейса
-  irs_u8 index; // иднекс строки, описывающей интерфейс
-} desc_interface;
+  /* Значения по умолчанию. */
+  desc_type = USB_DESC_TYPE_STRING;
 
-typedef struct : public basic_desc
+  /* Кодs поддерживаемых языков. */
+  list<irs_u16> langids;
+};
+
+struct desc_string : public basic_desc
+{
+  /* Значения по умолчанию. */
+  desc_type = USB_DESC_TYPE_STRING;  
+
+  /* Строка, закодированная в Unicode. */
+  string str;
+};
+
+struct desc_class : public basic_desc
 {
   irs_u8 lb_bcd; // младший байт версии класса
   irs_u8 hb_bcd; // страший байт версии класса
   irs_u8 contry_code; // код страны
   irs_u8 count_reports; // сколько будет дескрипторов report
-} desc_class;
+};
 
-typedef struct : public basic_desc
+struct desc_report : public basic_desc
 {
   /* TODO: Описатб структуру для report дескриптора */
-} desc_report;
+};
 
-typedef struct : public basic_desc
+struct desc_endpoint : public basic_desc
 {
-  irs_u8 address; // адрес конечной точки и направление 1 (IN) (be like 0x81)
-  irs_u8 attributes; // тип конечной точки. Не путать с desc_type!
-  irs_u8 lb_max_packet_size; // младший байт максимального размера пакета
-  irs_u8 hb_max_packet_size; // старший байт максимального размера пакета
-  irs_u8 interval_polling; // интервал опроса
-} desc_endpoint;
+  /* Значения по умолчанию */
+  length = 7;
+  desc_type = USB_DESC_TYPE_ENDPOINT;
 
-typedef struct : public basic_desc
-{
-  irs_u8 lobyte_bcd_usb; // младший байт версии usb, к-ую поддерживает устройство
-  irs_u8 hibyte_bcd_usb; // страший байт версии usb, к-ую поддерживает устройство
-  irs_u8 device_class; // класс по стандарту usb. Если указан 0x00, то класс отвечает сам за себя
-  irs_u8 device_subclass; // сабкласс по стандарту usb. Если указан 0x00, то сабкласс отвечает сам за себя
-  irs_u8 device_protocol; // протокол по стандарту usb. Если указан 0x00, то протокол отвечает сам за себя
-  irs_u8 max_packet_size; // максимальный размер пакета для endpoint 0 (при конфигурировании)
-  irs_u8 lobyte_vid; // младший байт vendor id
-  irs_u8 hibyte_vid; // старший байт vendor id
-  irs_u8 lobyte_product_id; // младший байт product id
-  irs_u8 hibyte_product_id; // старший байт product id
-  irs_u8 lobyte_version; // младший байт версии устройства (определяется программистом, как обычная версия программы)
-  irs_u8 hibyte_version; // старший байт версии устройства (определяется программистом, как обычная версия программы)
-  irs_u8 index_manufacturer; // индекс строки, описывающей производителя
-  irs_u8 index_product; // индекс строки, описывающей продукт
-  irs_u8 index_serial; // идекс строки, описывающей серию (продукта)
-  irs_u8 max_configuration_size; // максимальное количество конфигураций
-} desc_device;
+  /* Адрес конечной точки.
+   * Биты 0..3 - номер конечной точки,
+   * Биты 4..6 - зарезервированы, установлены в 0,
+   * Бит 7 - направление 0 = Out, 1 = In (для конечных точек игнорируется).
+   */
+  irs_u8 address;
 
-typedef struct : public basic_desc
-{
-  irs_u8 lb_total_length; // младший байт общего размера всего дерева под данной конфигурацией
-  irs_u8 hb_total_length; // старший байт общего размера всего дерева под данной конфигурацией
-  irs_u8 count_interfaces; // количество интерфейсов у данной конфигурации
-  irs_u8 index; // индекс данной конфигурации
-  irs_u8 index_str; // индекс строки, которая описывает данную конфигурацию
-  irs_u8* bm_attributes; // признак того, что устройство будет питаться от шины USB
-  irs_u8* max_power; // питание в амперах
-  desc_interface* interfaces; // дескрипторы интерфейсов
-  desc_class* classes; // дескрипторы классов
-  desc_endpoint* endpoints; // дескрипторы конечных точек
-} desc_configuration;
+  /* Набор битов, определяющие тип конечной точки.
+   * Биты 0..1 - тип передачи:
+   *   - 00 - Control,
+   *   - 01 - Isochronous,
+   *   - 10 - Bulk,
+   *   - 11 - Interrupt.
+   * Биты 2..7 - зарезервированы. Если конечная точка изохронная, то:
+   *   Биты 3..2 - тип синхронизации (режим Iso),
+   *     00 - No synchronisation,
+   *     01 - Asynchronous,
+   *     10 - Adaptive,
+   *     11 - Synchronous.
+   *   Биты 5..4 - тип использования Usage Type (режим Iso):
+   *     00 - коненая точка данных,
+   *     01 - конечная точка обратной связи (Feedback Endpoint),
+   *     10 - явная конечная точка обратной связи данных,
+   *     11 - зарезервировано.
+   */
+  irs_u8 attributes;
 
-class usb_module
+  /* Младший байт максимального размера пакета этой конечной точки,
+   * подходящий для отправки или приема.
+   */
+  irs_u8 lb_max_packet_size;
+
+  /* Старший байт максимального размера пакета этой конечной точки,
+   * подходящий для отправки или приема.
+   */
+  irs_u8 hb_max_packet_size;
+  
+  /* Интервал для того, чтобы опросить передачи данных конечной точки.
+   * Указывается в количестве фейрмов. Поле игнорируется для конечных точек
+   * Bulk и Control. Для конечных точек Isochronous должно быть равно 1 и
+   * для конечных точек interrupt может лежать в диапазоне 1..255. */
+  irs_u8 interval_polling;
+};
+
+struct desc_interface : public basic_desc
 {
+  /* Значения по умолчанию */
+  length = 9;
+  desc_type = USB_DESC_TYPE_INTERFACE;
+
+  /* Индекс (порядковый номер) интерфейса. */
+  irs_u8 number;
+
+  /* Величина, используемая для выбора лальтернативной установки.
+   * Нужен для того, чтобы показать, что настройка заимствуется
+   * у другого интерфейса. В качестве значения указывается
+   * индекс (порядковый номер) интерфейса, у к-ого заимствуется
+   * настройка.
+   */
+  irs_u8 alternate_setting;
+
+  /* Количество конечных точек, используемых в интерфейсе.
+   * Конечная точка 0 не учитывается.
+   */
+  irs_u8 count_endpoints;
+
+  /* Код класса (назначается организацией, следящей за стандартами USB). */
+  irs_u8 iclass;
+
+  /* Код подкласса (назначается организацией, следящей за стандартами USB). */
+  irs_u8 subclass;
+
+  /* Код протокола (назначается организацией, следящей за стандартами USB). */
+  irs_u8 protocol;
+
+  /* Индекс строкового дескриптора, описывающей этот интерфейс */
+  irs_u8 index_str;
+
+  /* Список конечных точек */
+  list<desc_endpoint> endpoints;
+};
+
+struct desc_device : public basic_desc
+{
+  /* Значения по умолчанию. */
+  length = 18;
+  desc_type = USB_DESC_TYPE_DEVICE;
+
+  /* Младший байт номера спецификации USB, с к-ой совместимо устройство. */
+  irs_u8 lobyte_bcd_usb;
+
+  /* Старший байт номера спецификации USB, с к-ой совместимо устройство. */
+  irs_u8 hibyte_bcd_usb;
+
+  /* Код класса (назначается организацией USB Org).
+   * Если равно 0, то каждый интерфейс указывает свой собственный код класса.
+   * Если равен 0xFF, то код класса определяяется вендором, иначе поле содержит
+   * код стандартного класса.
+   */
+  irs_u8 device_class;
+
+  /* Код подкласса (назначается организацией USB Org). */
+  irs_u8 device_subclass;
+
+  /* Код протокола (назначается организацией USB Org). */
+  irs_u8 device_protocol;
+
+  /* Максимальный размер пакета для конечной точки 0.
+   * Допустимые размеры: 8, 16, 32, 64.
+   */
+  irs_u8 max_packet_size;
+
+  /* Младший байт идентификатора вендора (VID)
+   * (назначается организацией USB Org).
+   */
+  irs_u8 lobyte_vid;
+
+  /* Старший байт идентификатора вендора (VID)
+   * (назначается организацией USB Org).
+   */
+  irs_u8 hibyte_vid;
+
+  /* Младший байт идентификатора продукта (PID)
+   * (назначается организацией - производителем)
+   */
+  irs_u8 lobyte_product_id;
+
+  /* Старший байт идентификатора продукта (PID)
+   * (назначается организацией - производителем)
+   */
+  irs_u8 hibyte_product_id;
+
+  /* Младший байт номера версии устройства (задается разработчиком устройства). */
+  irs_u8 lobyte_version;
+
+  /* Страший байт номера версии устройства (задается разработчиком устройства). */
+  irs_u8 hibyte_version;
+
+  /* Индекс строки, описывающей прозиводителя
+   * (необязательное поле, можно указать 0)
+   */
+  irs_u8 index_manufacturer;
+
+  /* Индекс строки, описывающей продукт
+   * (необязательное поле, можно указать 0)
+   */
+  irs_u8 index_product;
+
+  /* Индекс строки, содержащей серийный номер
+   * (необязательное поле, можно указать 0)
+   */
+  irs_u8 index_serial;
+
+  /* Количество возможных конфигураций */
+  irs_u8 max_configuration_size;
+};
+
+struct desc_configuration : public basic_desc
+{
+  /* Значения по умолчанию */
+  desc_type = USB_DESC_TYPE_CONFIGURATION;
+
+  /* Младший байт полной длины возвращаемых данных в байтах.
+   * (общий размер всего деерва под данной конфигурацией)
+   */
+  irs_u8 lb_total_length;
+
+  /* Старший байт полной длины возвращаемых данных в байтах.
+   * (общий размер всего деерва под данной конфигурацией)
+   */
+  irs_u8 hb_total_length;
+
+  /* Количество интерфейсов. */
+  irs_u8 count_interfaces;
+
+  /* Величина, используемая как аргумент для выбора этой конфигурации. */
+  irs_u8 value;
+
+  /* Индекс строкового дескриптора, описывающего эту конфигурацию. */
+  irs_u8 index_str;
+
+  /* Признак того, что устройство будет питаться от шины USB.
+   * D7 зарезервировано, установлено в 1. (USB 1.0 Bs Powered),
+   * D6 самозапитываемое (Self Powered),
+   * D5 удаленное пробуждение (Remote Wakeup),
+   * D4..0 зарезервировано, установлено в 0.
+   */
+  irs_u8* bm_attributes;
+
+  /* Максимальное энергопотребление в единицах 2 мА. */
+  irs_u8* max_power;
+
+  /* Список интерфейсов данной кофигурации. */
+  list<desc_interface> interfaces;
+
+  /* Список классов данной конфигурации. */
+  list<desc_class> classes;
+};
+
+class basic_usb_module
+{
+public:
+  basic_usb_module(desc_configuration desc_conf);
+  ~basic_usb_module();
+
+  desc_configuration get_desc_conf() const;
+
 private:
+  /* TODO: Конфигурация определяется модулем или девайсом? */
+  desc_configuration m_desc_conf;
+};
 
-}
+basic_usb_module::basic_usb_module(desc_configuration desc_conf)
+  : m_desc_conf(desc_conf)
+{}
+
+basic_usb_module::~basic_usb_module()
+{}
+
+desc_configuration basic_usb_module::get_desc_conf() const
+{ return m_desc_conf; }
 
 class usb_composite_device
 {
@@ -284,8 +470,9 @@ private:
 
   bool m_is_enabled;
 
-  list<usb_module*> m_modules_list;
-}
+  list<basic_usb_module*> m_modules_list;
+  desc_device m_desc_device;
+};
 
 usb_composite_device::usb_composite_device()
   : m_vendor_id(IRS_VENDOR_ID)
