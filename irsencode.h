e5ac2577cde41957fbd8b8143c4c4769a7a831cd
qucals <totalboy@outlook.com>
2021-08-27 16:50:11 +0500

    Исправлены баги с буфферами
    - В lwipopts_conf добавлен LWIP_RAM_HEAP_POINTER

--- irsencode.h
+++ irsencode.h
@@ -1,9 +1,7 @@
-// @brief функции перевода строк в различные кодировки.
+// @brief функции перевода строк в различнык кодировки
 //
-// Дата: 18.04.2021
+// Дата: 27.08.2021
 // Дата создания: 12.04.2021
-
-#pragma once
 
 #ifndef ENCODE_H
 #define ENCODE_H
@@ -19,16 +17,25 @@
 
 namespace irs {
 
-/* @brief функция перевода строки из кодировки cp1251 в utf8.
- * 
- * @param ap_out - указатель на строку, в которую будет записываться
- * перекодированные данные.
- * @param ap_in - указатель на строку в кодировке cp1251.
+/**
+ * @brief функция перевода строки из кодировки cp1251 в utf8.
+ *
+ * @param a_in: ссылка на вектор символов в кодировке cp1251.
+ * @param a_size_in: количество считываемых символов из входного вектора.
+ * @param a_out: ссылка на вектор для перекодированных символов в utf8.
+ *
+ * @return size_t: количество вставленных символов в a_out
  */
-inline void cp1251_to_utf8(char* ap_out, size_t a_out_size, const char* ap_in)
+inline size_t cp1251_to_utf8(const vector<char>& a_in, const size_t a_size_in,
+  vector<char>& a_out
+)
 {
-  IRS_ASSERT(ap_out == NULL || ap_in == NULL);
-  
+  if (a_size_in > a_in.size()) {
+#ifndef IRS_NOEXCEPTION
+    throw runtime_error("a_size_in cannot be bigger than a_in.size()");
+#endif // IRS_NOEXCEPTION
+  }
+
   static const char table[128*3 + 1] = {
     "\320\202 \320\203 \342\200\232\321\223 \342\200\236\342\200\246\342\200\240\342\200\241"
     "\342\202\254\342\200\260\320\211 \342\200\271\320\212 \320\214 \320\213 \320\217 "
@@ -48,45 +55,51 @@ inline void cp1251_to_utf8(char* ap_out, size_t a_out_size, const char* ap_in)
     "\321\210 \321\211 \321\212 \321\213 \321\214 \321\215 \321\216 \321\217 "
   };
 
-  while (*ap_in && (a_out_size--) > 0) {
-    if (*ap_in & 0x80) {
-      const char *p = &table[3 * (0x7f & *ap_in++)];
+  size_t j = 0;
+
+  for (size_t i = 0; i < a_size_in && i < a_in.size(); i++) {
+    if (a_in.at(i) & 0x80) {
+      const char* p = &table[3 * (0x7f & a_in.at(i))];
+
       if (*p == ' ') { continue; }
-      *ap_out++ = *p++;
-      *ap_out++ = *p++;
+
+      a_out.at(j++) = *p++;
+      a_out.at(j++) = *p++;
+
       if (*p == ' ') { continue; }
-      *ap_out++ = *p++;
+
+      a_out.at(j++) = *p++;
     } else {
-      *ap_out++ = *ap_in++;
+      a_out.at(j++) = a_in.at(i);
     }
   }
 
-  *ap_out = 0;
+  return j;
 }
 
 #if IRS_USE_UTF8_CPP
-// TODO: Реализовать функцию с параметрами const char*, char*.
-/* @brief функция перевода строки из кодировки UTF-16 (UTF-32) в UTF-8. Функция
- * определяет размерность wchar_t и взаивисимости от этого определяет начальную
- * кодировку. В случае, если размерность не равна 2 или 4, выдает ошибку.
- * 
- * @param a_str - строка в кодировке UTF-16 (UTF-32).
+/**
+ * @brief функция перевода строки из кодировки UTF-16 (UTF-32) в UTF-8. Функция
+ * опеределят размерность wchat_t и в зависимости от этого определяет начальную
+ * кодировку. В случае, если размерность не равна ни 2, ни 4 - выдает ошибку.
  *
- * @return string - перекодированная строка в UTF-8.
+ * @param a_str - ссылка на строку в кодировке UTF-16 (UTF-32).
+ * @param a_size_in - количество кодируемых символов.
+ * @param a_out - ссылка на выходной вектор.
  */
-inline string wstring_to_utf8(const wstring& a_str)
+// const wstring& a_str
+inline void wstring_to_utf8(const vector<wchar_t>& a_in, const size_t a_size_in,
+  vector<char>& a_out)
 {
-   string str_utf8;
-   
    if (sizeof(wchar_t) == 2) {
-    utf8::unchecked::utf16to8(a_str.begin(), a_str.end(), back_inserter(str_utf8));
+    utf8::unchecked::utf16to8(a_in.begin(), a_in.begin() + a_size_in,
+      back_inserter(a_out));
   } else if (sizeof(wchar_t) == 4) {
-    utf8::unchecked::utf32to8(a_str.begin(), a_str.end(), back_inserter(str_utf8));
+    utf8::unchecked::utf32to8(a_in.begin(), a_in.begin() + a_size_in,
+      back_inserter(a_out));
   } else {
     IRS_STATIC_ASSERT((sizeof(wchar_t) == 2) || (sizeof(wchar_t) == 4));
   }
-  
-  return str_utf8;
 }
 #endif // IRS_USE_UTF8_CPP
 
--- irslwipbuf.h
+++ irslwipbuf.h
@@ -2,9 +2,7 @@
 // в качестве передачи данных.
 //
 // Дата: 13.08.2021
-// Дата создания: 12.04.2021
-
-#pragma once
+// Дата создания: 26.04.2021
 
 #ifndef LWIP_BUF_H
 #define LWIP_BUF_H
@@ -21,26 +19,23 @@
 #include <irsnetdefs.h>
 #include <irsencode.h>
 #include <irsstrconv.h>
-#include <lwipopts_conf.h>
 
 #ifdef USE_LCD
-#ifdef __cplusplus
 extern "C" {
-#endif // __cplusplus
-
 #include "lcd_trace.h"
-
-#ifdef __cplusplus
 }
-#endif // __cplusplus
 #endif // USE_LCD
 
 #pragma diag_suppress=Pa181
+
+#include "lwipopts.h"
+
 #include "netif/etharp.h"
 
 #include "lwip/opt.h"
 #include "lwip/init.h"
 #include "lwip/netif.h"
+
 #ifdef IRS_STM32F4xx
 #include "lwip/timers.h"
 #else //IRS_STM32F4xx
@@ -50,7 +45,8 @@ extern "C" {
 
 #include "lwip/stats.h"
 #include "lwip/tcp.h"
-#pragma diag_default=Pa181  
+
+#pragma diag_default=Pa181
 
 #define _OVERRIDE_
 
@@ -58,7 +54,7 @@ namespace irs
 {
 
 /* Размер LWIP буфера по умолчанию */
-#define IRSLIB_LWIPBUF_SIZE 128
+#define IRSLIB_LWIPBUF_SIZE 5
 
 template<typename char_type, typename traits_type = char_traits<char_type>>
 class basic_lwipbuf: public basic_streambuf<char_type, traits_type>
@@ -174,14 +170,15 @@ private:
   int tcp_init();
 
   /**
-   * @brief функция копирования строки в m_temp_buffer с корректными '\n', а
+   * @brief функция копирования строки в m_copy_buffer с корректными '\n', а
    * именно функция добавляет перед каждым '\n' символ '\r'.
    *
    * @param ap_str - строка, которую необходимо скопировать.
+   * @param a_size_str - количество считываемых символов
    *
    * @return size_t - получившийся размер строки вместе с добавленными символами.
    */
-  size_t copy_str_to_buffer_with_correct_endls(const char* ap_str);
+  size_t copy_with_r_symbols(const char* ap_str, const size_t a_size_str);
 
   /**
    * @brief callback-функция получения дескриптора нового соединения.
@@ -245,26 +242,29 @@ private:
   const ip_addr_t* mp_ip;
 
   /**
-   * Буфер, используемый для перевода данных из одного кодировки в другую.
-   * По умолчанию размерность данного буфера равняется m_sizebuf*2. В случае
-   * использования > UTF-8 размерность задается m_sizebuf * sizeof(char_type).
+   * Буфер, используемый для перевода данных из одного кодировки в другую при
+   * использовании char_type - wchar_t.
    */
-  char* mp_convert_buffer;
+  vector<char> m_encoding_conversion_buffer;
 
   /**
    * Глваный буфер, в который происходит заполнение данных в случае
    * использования потоков ввода/вывода.
    */
-  vector<char_type> m_buffer;
+  vector<char_type> m_stream_buffer;
 
   /**
    * Промежуточный буфер, в который происходит копирование данных из главного
    * буфера, заменяя символы '\n' на "\r\n" для корректного перехода курсора
    * дисплея на новую строку.
-   * По умолчанию размерность данного буфера равняется m_sizebuf*2. В случае
-   * использования > UTF-8 размерность задается m_sizebuf * sizeof(char_type).
+   * Размерность вектора задается в зависимости от используемого char_type.
+   * В случае использования char (sizeof(char) = 1), то выделение памяти
+   * происходит по формуле: m_sizebuf * 3, где число 3 означает возможную
+   * размерность одного символа в новой кодировке.
+   * В случае использования другого типа данных, отличного от char, вместо
+   * числа 3 используется число 6 (основываясь на wchar_t).
    */
-  vector<char> m_temp_buffer;
+  vector<char> m_copy_buffer;
 
   /* Массив, хранящий все дескрипторы активных соединений. */
   arr_conn_type m_connections;
@@ -283,15 +283,24 @@ basic_lwipbuf<char_type, traits_type>::basic_lwipbuf(size_t a_sizebuf,
   : m_sizebuf(a_sizebuf)
   , m_port(a_port)
   , mp_ip(ap_ip)
-  , mp_convert_buffer(new char[m_sizebuf * sizeof(char_type)]())
-  , m_buffer(m_sizebuf)
-  , m_temp_buffer(m_sizebuf * sizeof(char_type))
+  , m_encoding_conversion_buffer()
+  , m_stream_buffer(m_sizebuf)
+  , m_copy_buffer()
   , m_connected(false)
 {
+  if (sizeof(char_type) == 1) {
+    m_copy_buffer.resize(m_sizebuf * 2);
+    m_encoding_conversion_buffer.resize(m_sizebuf * 3);
+  } else {
+    m_copy_buffer.resize(m_sizebuf * 4);
+    m_encoding_conversion_buffer.resize(m_sizebuf * 4);
+  }
+
   /**
    * Задаем буфер, в к-ые будут сохраняться данные, передаваемые через
-   * потоки ввода/вывода. */
-  this->setp(m_buffer.data(), m_buffer.data() + m_buffer.size());
+   * потоки ввода/вывода.
+   */
+  this->setp(m_stream_buffer.data(), m_stream_buffer.data() + m_stream_buffer.size());
 
   /* Инициализация сервера. */
   if (tcp_init() < 0) {
@@ -308,7 +317,6 @@ basic_lwipbuf<char_type, traits_type>::basic_lwipbuf(size_t a_sizebuf,
 template<typename char_type, typename traits_type>
 basic_lwipbuf<char_type, traits_type>::~basic_lwipbuf()
 {
-  if (mp_convert_buffer) { delete[] mp_convert_buffer; }
   if (mp_tcp_pcb) { memp_free(MEMP_TCP_PCB, mp_tcp_pcb); }
 }
 
@@ -318,17 +326,16 @@ int basic_lwipbuf<char_type, traits_type>::tcp_init()
   /* Создаем новый дескриптор соединения. */
   mp_tcp_pcb = tcp_new();
 
-  if (mp_tcp_pcb != NULL) {
-    /* Биндим дескриптор на определенный порт. */
-
+  if (mp_tcp_pcb != IRS_NULL) {
     if (!mp_ip) {
 #ifndef IRS_NOEXCEPTION
-      throw runtime_error("mp_ip cannot be nullptr");
+      throw runtime_error("mp_ip cannot be IRS_NULL");
 #else // IRS_NOEXCEPTION
       return -1;
 #endif // IRS_NOEXCEPTION
     }
 
+    /* Биндим дескриптор на определенный порт. */
     err_t err = tcp_bind(mp_tcp_pcb, const_cast<ip_addr_t*>(mp_ip), m_port);
     if (err == ERR_OK) {
       /* Начинаем слушать текущий порт на новые соединения. */
@@ -362,8 +369,8 @@ void basic_lwipbuf<char_type, traits_type>::tick()
 
 template<typename char_type, typename traits_type>
 void
-basic_lwipbuf<char_type, traits_type>::send(const void* ap_msg,
-  size_t a_size_msg
+basic_lwipbuf<char_type, traits_type>::send(
+  const void* ap_msg, size_t a_size_msg
 )
 {
 #ifdef USE_LCD
@@ -413,16 +420,16 @@ size_t basic_lwipbuf<char_type, traits_type>::get_count_connected() const
 
 template<typename char_type, typename traits_type>
 size_t
-basic_lwipbuf<char_type, traits_type>::copy_str_to_buffer_with_correct_endls(
-  const char* ap_str
+basic_lwipbuf<char_type, traits_type>::copy_with_r_symbols(
+  const char* ap_str, const size_t a_size_str
 )
 {
   if (ap_str) {
     size_t j = 0;
 
-    for (size_t i = 0; i < strlen(ap_str); i++) {
-      if (ap_str[i] == '\n') { m_temp_buffer[j++] = '\r'; }
-      m_temp_buffer[j++] = ap_str[i];
+    for (size_t i = 0; i < a_size_str; i++) {
+      if (ap_str[i] == '\n') { m_copy_buffer.at(j++) = '\r'; }
+      m_copy_buffer.at(j++) = ap_str[i];
     }
 
     return j;
@@ -437,7 +444,9 @@ basic_lwipbuf<char_type, traits_type>::overflow(int_type c)
 {
 #ifndef IRS_NOEXCEPTION
   throw runtime_error("There is no overloaded overflow function for the type used");
-#endif // IRS_NOEXCEPTION
+#else
+  return -1
+#endif // // IRS_NOEXCEPTION
 }
 
 template<>
@@ -448,15 +457,16 @@ basic_lwipbuf<wchar_t>::int_type basic_lwipbuf<wchar_t>::overflow(int_type c)
   ptrdiff_t sz = this->pptr() - this->pbase();
 
   /* Осуществляем перевод кодировок. */
-  wstring temp(L"", sz);
-  for (size_t i = 0; i < sz; i++) { temp[i] = m_buffer[i]; }
-  string converted_str = wstring_to_utf8(temp);
+  wstring_to_utf8(m_stream_buffer, sz, m_encoding_conversion_buffer);
 
-  size_t msg_size = copy_str_to_buffer_with_correct_endls(
-    converted_str.c_str());
+  size_t msg_size = copy_with_r_symbols(
+    &m_encoding_conversion_buffer.front(), m_encoding_conversion_buffer.size()
+  );
 
   /* Отправляем данные клиентам. */
-  send(&m_temp_buffer.front(), msg_size);
+  send(&m_copy_buffer.front(), msg_size);
+
+  m_encoding_conversion_buffer.clear();
 
   /**
    * Устанавлием позицию каретки заполнения данных для корректной записи данных
@@ -465,7 +475,7 @@ basic_lwipbuf<wchar_t>::int_type basic_lwipbuf<wchar_t>::overflow(int_type c)
   this->pbump(-sz);
 
   if (c != traits_type::eof()) {
-    m_buffer[0] = static_cast<wchar_t>(c);
+    m_stream_buffer[0] = static_cast<wchar_t>(c);
     this->pbump(1);
   }
 
@@ -479,13 +489,16 @@ basic_lwipbuf<char>::int_type basic_lwipbuf<char>::overflow(int_type c)
   /* Вычисляем размер сообщения в буфере. */
   ptrdiff_t sz = this->pptr() - this->pbase();
 
-  /* Осуществляем перевод кодировок. */
-  cp1251_to_utf8(mp_convert_buffer, sz, &m_buffer.front());
+  /* Добавляем к каждому символу '/n' символ '/r' */
+  // size_t msg_size = copy_with_r_symbols(&m_stream_buffer.front(), sz);
+  size_t msg_size = copy_with_r_symbols(&m_stream_buffer.front(), sz);
 
-  size_t msg_size = copy_str_to_buffer_with_correct_endls(mp_convert_buffer);
+  /* Осуществляем перевод кодировок. */
+  msg_size = cp1251_to_utf8(m_copy_buffer, msg_size, 
+    m_encoding_conversion_buffer);
 
   /* Отправляем данные клиентам. */
-  send(&m_temp_buffer.front(), msg_size);
+  send(m_encoding_conversion_buffer.c_str(), msg_size);
 
   /**
    * Устанавлием позицию каретки заполнения данных для корректной записи данных
@@ -494,7 +507,7 @@ basic_lwipbuf<char>::int_type basic_lwipbuf<char>::overflow(int_type c)
   this->pbump(-sz);
 
   if (c != traits_type::eof()) {
-    m_buffer[0] = static_cast<char>(c);
+    m_stream_buffer[0] = static_cast<char>(c);
     this->pbump(1);
   }
 
@@ -556,7 +569,7 @@ err_t basic_lwipbuf<char_type, traits_type>::c_tcp_recv(void* arg,
    * Если никаких пакетов не было получено от клиента, значит он отключился.
    * В этом случае осуществляем удаление дескриптора данного соединения.
    */
-  if (p == NULL) {
+  if (p == IRS_NULL) {
     c_tcp_connection_close(tpcb);
 
     arr_conn_type::iterator iter = find(conns->begin(), conns->end(), tpcb);
@@ -584,10 +597,10 @@ basic_lwipbuf<char_type, traits_type>::c_tcp_connection_close(
 )
 {
   /* Удаляем все callback-функции для данного дескриптора. */
-  tcp_arg(tpcb, NULL);
-  tcp_sent(tpcb, NULL);
-  tcp_recv(tpcb, NULL);
-  tcp_poll(tpcb, NULL, 0);
+  tcp_arg(tpcb, IRS_NULL);
+  tcp_sent(tpcb, IRS_NULL);
+  tcp_recv(tpcb, IRS_NULL);
+  tcp_poll(tpcb, IRS_NULL, 0);
 
   /* Закрываем TCP соединение. */
   tcp_close(tpcb);
