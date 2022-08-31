ddca7eb2d5fc995361a88c84db587629eb897457
Андрей Владимирович Кравчук <508@irs.local>
2022-08-31 18:26:24 +0500

    Реализована отправка данных по I2C (проверено только на индикаторе)

--- irsi2c.cpp
+++ irsi2c.cpp
@@ -17,10 +17,9 @@ irs::arm::arm_i2c_t::arm_i2c_t()
   GPIOA->OUTMODE |= GPIO_OUTMODE_PIN0_OD << GPIO_OUTMODE_PIN0_Pos;
   GPIOA->OUTMODE |= GPIO_OUTMODE_PIN1_OD << GPIO_OUTMODE_PIN1_Pos;
   GPIOA->DENSET |= GPIO_DENSET_PIN0_Msk | GPIO_DENSET_PIN1_Msk;
-
   RCU_APBRstCmd(RCU_APBRst_I2C, ENABLE);
   RCU_APBClkCmd(RCU_APBClk_I2C, ENABLE);
-  I2C_FSFreqConfig(100000, RCU_GetSysClkFreq());
+  I2C_FSFreqConfig(400000, RCU_GetSysClkFreq());
   I2C_Cmd(ENABLE);
 }
 
@@ -28,27 +27,27 @@ irs::arm::arm_i2c_t::~arm_i2c_t()
 {
   RCU_APBRstCmd(RCU_APBRst_I2C, DISABLE);
   RCU_APBClkCmd(RCU_APBClk_I2C, DISABLE);
-//  GPIOA->ALTFUNCSET |= GPIO_ALTFUNCSET_PIN0_Msk | GPIO_ALTFUNCSET_PIN1_Msk;
-//  GPIOA->PULLMODE |= GPIO_PULLMODE_PIN0_PU << GPIO_PULLMODE_PIN0_Pos;
-//  GPIOA->PULLMODE |= GPIO_PULLMODE_PIN1_PU << GPIO_PULLMODE_PIN1_Pos;
-//  GPIOA->OUTMODE |= GPIO_OUTMODE_PIN0_OD << GPIO_OUTMODE_PIN0_Pos;
-//  GPIOA->OUTMODE |= GPIO_OUTMODE_PIN1_OD << GPIO_OUTMODE_PIN1_Pos;
-//  GPIOA->DENSET |= GPIO_DENSET_PIN0_Msk | GPIO_DENSET_PIN1_Msk;
-  NVIC_DisableIRQ(I2C_IRQn);
+  GPIOA->ALTFUNCCLR |= GPIO_ALTFUNCCLR_PIN0_Msk | GPIO_ALTFUNCCLR_PIN1_Msk;
+  GPIOA->PULLMODE &= ~GPIO_PULLMODE_PIN0_Msk;
+  GPIOA->PULLMODE &= ~GPIO_PULLMODE_PIN1_Msk;
+  GPIOA->OUTMODE &= ~GPIO_OUTMODE_PIN0_Msk;
+  GPIOA->OUTMODE &= ~GPIO_OUTMODE_PIN1_Msk;
+  GPIOA->DENCLR |= GPIO_DENCLR_PIN0_Msk | GPIO_DENCLR_PIN1_Msk;
+  I2C_Cmd(DISABLE);
 }
 
-void irs::arm::arm_i2c_t::i2c_init()
-{
-
+//void irs::arm::arm_i2c_t::i2c_init()
+//{
+//
 //  while (I2C_BusBusyStatus()) asm ("nop");
 //
 //  if(I2C->CST_bit.TSDA)
 //    I2C->CST |= I2C_CST_TGSCL_Msk;
-
-
-
+//
+//
+//
 //  for (int i=0; i<100000;i++) asm ("nop");
-
+//
 //  do {
 //  RCU_APBRstCmd(RCU_APBRst_I2C, DISABLE);
 //  RCU_APBClkCmd(RCU_APBClk_I2C, DISABLE);
@@ -68,8 +67,7 @@ void irs::arm::arm_i2c_t::i2c_init()
 //
 //  for (int i=0; i<100000;i++) {asm ("nop");}
 //  } while (I2C_GetState() != I2C_ST_MODE_STDONE);
-}
-
+//}
 
 void irs::arm::arm_i2c_t::lock()
 {
@@ -91,128 +89,97 @@ void irs::arm::arm_i2c_t::set_device_addr(irs_u16 a_device_addr)
   m_device_address = a_device_addr;
 }
 
-void irs::arm::arm_i2c_t::read(irs_u16 a_mem_addr, uint8_t *ap_buf, irs_u16 a_size)
+void irs::arm::arm_i2c_t::read(uint8_t *ap_buf, irs_u16 a_size)
 {
-  int mode = 1;
-  int msg = 0;
-
-
   this->lock();
   m_status = BUSY_READ;
-  this->i2c_init();
-  //start counter of bus error generation
-   I2C_StartCmd();
+  I2C_Cmd(ENABLE);
+  I2C_StartCmd();
+
+  irs_u16 temp_variable = 0;
   while (a_size)
   {
     while (!(I2C->ST_bit.INT)) { asm ("nop"); }
 
-    uint32_t i2c_state = I2C->ST & I2C_ST_MODE_Msk;
-    switch (i2c_state) {
+    switch (I2C->ST & I2C_ST_MODE_Msk) {
+
       case I2C_ST_MODE_STDONE: { // start condition generated
-         switch (this->get_status()){
-          case BUSY_READ: {
-          I2C_SetData (m_device_address);
-          I2C_ITStatusClear();
-          } break;
-          case BUSY_WRITE: {
-          I2C_SetData (m_device_address);
-          I2C_ITStatusClear();
-          } break;
-        }
-      }
-      break;
-
-
-
-
-
-
-    case I2C_ST_MODE_MTADNA: // //mt slave ad sent NACK
-       I2C_StopCmd();
-       I2C_SetData (m_device_address);
-       I2C_StartCmd();
-       I2C_ITStatusClear();
-       m_device_address+=2;
-      break;
-
-      case 14 : break;// byte sent
-    default:
-
-      msg++;
-    ((msg == 1) && (mode == 1)) ? (I2C_SetData (0x06),1) : 1;
-    ((msg == 2) && (mode == 1)) ? (I2C_SetData (0x00),1) : 1;
-    ((msg == 3) && (mode == 1)) ? (I2C_SetData (0x00), I2C_StartCmd(),
-                                   I2C_SetData (m_device_address), msg = 0, mode++) : 1;
-    ((msg == 1) && (mode == 2)) ? (I2C_SetData (0x02),1) : 1;
-    ((msg == 2) && (mode == 2)) ? (I2C_SetData (0xFF),1) : 1;
-    ((msg == 3) && (mode == 2)) ? (I2C_SetData (0x03),1) : 1;
-
-    I2C_ITStatusClear();
-
-
-
-
-
-
-
-
-
-
-      I2C_SetData (m_device_address);
+      I2C_SetData (m_device_address | 0x01);
       I2C_ITStatusClear();
-      break;  //error
+      }  break;
+
+      case I2C_ST_MODE_MTADNA:  { // //mt slave ad sent NACK
+      I2C_StopCmd();
+      I2C_SetData (m_device_address | 0x01);
+      I2C_StartCmd();
+      I2C_ITStatusClear();
+      }  break;
+
+      case I2C_ST_MODE_MRADPA:
+        I2C_SetData (0x02);
+        I2C_ITStatusClear();
+      case I2C_ST_MODE_MRDAPA:  {
+      reinterpret_cast <irs_u32&>(*(ap_buf+temp_variable)) = I2C_GetData();
+      I2C_ITStatusClear();
+      a_size--;
+      temp_variable++;
+      }  break;
+      default:
+      for (;;){}
+      break;
 
     }
   }
+  for (int i=0; i<500; i++) { asm ("nop"); } // bus free time. 407 is min for 400kHz
+                            // for more information read datasheet of i2c slave
+  I2C_StopCmd();
+  I2C_Cmd(DISABLE);
   this->unlock();
   m_status = READY;
 }
 
-void irs::arm::arm_i2c_t::write(irs_u16 a_mem_addr, uint8_t *ap_buf,
-  irs_u16 a_size)
+void irs::arm::arm_i2c_t::write(uint8_t *ap_buf, irs_u16 a_size)
 {
   this->lock();
   m_status = BUSY_WRITE;
+  I2C_Cmd(ENABLE);
+  I2C_StartCmd();
 
-  do {
-    this->i2c_init();
-       // rename to init of bus condition, not all interface
-  } while (I2C->ST_bit.MODE != I2C_ST_MODE_STDONE) ;
-  //start counter of bus error generation
-while (a_size)
+  irs_u16 temp_variable = 0;
+  while (a_size)
   {
-    while (!I2C->ST_bit.INT) { asm ("nop"); }
+    while (!(I2C->ST_bit.INT)) { asm ("nop"); }
 
-    uint32_t i2c_state = I2C->ST & I2C_ST_MODE_Msk;
-    switch (i2c_state) {
+    switch (I2C->ST & I2C_ST_MODE_Msk) {
+
       case I2C_ST_MODE_STDONE: { // start condition generated
-         switch (this->get_status()){
-          case BUSY_READ: {
-          I2C_SetData (m_device_address | 0x01);
-          I2C_ITStatusClear();
-          } break;
-          case BUSY_WRITE: {
-          I2C_SetData (m_device_address);
-          I2C_ITStatusClear();
-          } break;
-        }
-      }
-      break;
-    case I2C_ST_MODE_MTADPA: //master transmit Slave address sent, positive ACK
+      I2C_SetData (m_device_address);
+      I2C_ITStatusClear();
+      }  break;
 
-      break; //address sent
+      case I2C_ST_MODE_MTADNA:  { // //mt slave ad sent NACK
+      I2C_StopCmd();
+      I2C_SetData (m_device_address);
+      I2C_StartCmd();
+      I2C_ITStatusClear();
+      }  break;
 
-      case 14 : break;// byte sent
-    default: break;  //error
-    //  I2C_ST_MODE_MTADNA                         = 0x5UL,                /*!< FS master transmit - Slave address sent, negative ACK */
-
+      case I2C_ST_MODE_MTADPA:
+      case I2C_ST_MODE_MTDAPA:  {
+      I2C_SetData (reinterpret_cast <irs_u32&>(*(ap_buf+temp_variable)));
+      I2C_ITStatusClear();
+      a_size--;
+      temp_variable++;
+      }  break;
+      default: {
+      for (;;){ asm ("nop"); }
+      }  break;
     }
   }
-
-
-
-
-  //  w=0
+  for (int i=0; i<500; i++) { asm ("nop"); } // bus free time. 407 is min for 400kHz
+                            // for more information read datasheet of i2c slave
+  I2C_StopCmd();
+  I2C_Cmd(DISABLE);
   this->unlock();
   m_status = READY;
 }
--- irsi2c.h
+++ irsi2c.h
@@ -8,12 +8,9 @@
 
 #include <irsdefsbase.h>
 
-
-
 #define I2C_CHIP_ADDRESS 0xC8UL //check for correct or not
 #define I2C_WRITE 0x00UL
 #define I2C_READ 0x01UL
-
 
 namespace irs
 {
@@ -26,21 +23,19 @@ class i2c_t
 public:
   enum status_t {READY, BUSY_READ, BUSY_WRITE};
 
+  virtual ~i2c_t() {}
   virtual void lock() = 0;
   virtual void unlock() = 0;
   virtual bool get_lock() = 0;
-
-  virtual ~i2c_t() {}
   virtual void set_device_addr(irs_u16 a_device_addr) = 0;
-  virtual void read(irs_u16 a_mem_addr, uint8_t *ap_buf, irs_u16 a_size) = 0;
-  virtual void write(irs_u16 a_mem_addr, uint8_t *ap_buf, irs_u16 a_size) = 0;
+  virtual void read(uint8_t *ap_buf, irs_u16 a_size) = 0;
+  virtual void write(uint8_t *ap_buf, irs_u16 a_size) = 0;
   virtual status_t get_status() = 0;
   virtual void tick() = 0;
 };
 
 namespace arm
 {
-
 #if defined (IRS_NIIET_1921)
 
 class arm_i2c_t : public i2c_t
@@ -52,24 +47,20 @@ public:
   void unlock();
   bool get_lock();
   void set_device_addr(irs_u16 a_device_addr);
-  void read(irs_u16 a_mem_addr, uint8_t *ap_buf, irs_u16 a_size);
-  void write(irs_u16 a_mem_addr, uint8_t *ap_buf, irs_u16 a_size);
+  void read(uint8_t *ap_buf, irs_u16 a_size);
+  void write(uint8_t *ap_buf, irs_u16 a_size);
   status_t get_status();
   void tick();
 private:
-  void i2c_init(void);
   bool m_lock;
   status_t m_status;
   irs_u16 m_device_address;
-//  m_mem_addr_size;
 };
 
 #else
   #error Тип контроллера не определён
 #endif //defined (IRS_NIIET_1921)
-
 } //namespace arm
-
 
 //! @}
 
