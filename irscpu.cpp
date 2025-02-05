707a205cb93c14618a787bc8e82215b3e5b1fd16
KrasheninnikovMaxim <maxk008@gmail.com>
2025-02-05 15:43:35 +0500

    irscpu.cpp Применен clang-format 20

--- irscpu.cpp
+++ irscpu.cpp
@@ -23,75 +23,66 @@ irs::cpu_traits_t::frequency_type irs::cpu_traits_t::m_frequency = 16000000;
 #elif __ICCARM__
 #if defined(__LM3Sx9xx__)
 irs::cpu_traits_t::frequency_type irs::cpu_traits_t::m_frequency = 50000000;
-#elif defined (__LM3SxBxx__)
+#elif defined(__LM3SxBxx__)
 irs::cpu_traits_t::frequency_type irs::cpu_traits_t::m_frequency = 80000000;
 #elif defined(__STM32F100RBT__)
 irs::cpu_traits_t::frequency_type irs::cpu_traits_t::m_frequency = 8000000;
 #elif defined(IRS_STM32F2xx)
 irs::cpu_traits_t::frequency_type irs::cpu_traits_t::m_frequency = 120000000;
-irs::cpu_traits_t::frequency_type
-  irs::cpu_traits_t::m_periphery_frequency_first = 30000000;
-irs::cpu_traits_t::frequency_type
-  irs::cpu_traits_t::m_periphery_frequency_second = 60000000;
+irs::cpu_traits_t::frequency_type irs::cpu_traits_t::m_periphery_frequency_first = 30000000;
+irs::cpu_traits_t::frequency_type irs::cpu_traits_t::m_periphery_frequency_second = 60000000;
 double irs::cpu_traits_t::m_flash_voltage = 3.3;
 #elif (defined(IRS_STM32F4xx) || defined(IRS_STM32F7xx))
 irs::cpu_traits_t::frequency_type irs::cpu_traits_t::m_frequency = 16000000;
-irs::cpu_traits_t::frequency_type
-  irs::cpu_traits_t::m_periphery_frequency_first = 16000000;
-irs::cpu_traits_t::frequency_type
-  irs::cpu_traits_t::m_periphery_frequency_second = 16000000;
+irs::cpu_traits_t::frequency_type irs::cpu_traits_t::m_periphery_frequency_first = 16000000;
+irs::cpu_traits_t::frequency_type irs::cpu_traits_t::m_periphery_frequency_second = 16000000;
 double irs::cpu_traits_t::m_flash_voltage = 3.3;
-#else // Другой __ICCARM__
+#else  // Другой __ICCARM__
 irs::cpu_traits_t::frequency_type irs::cpu_traits_t::m_frequency = 100000000;
 #endif // ARM_device
-#else //__ICCAVR
+#else  //__ICCAVR
 irs::cpu_traits_t::frequency_type irs::cpu_traits_t::m_frequency = 1;
 #endif //__ICCAVR__
 void irs::cpu_traits_t::frequency(frequency_type a_frequency)
 {
   m_frequency = a_frequency;
 }
+
 irs::cpu_traits_t::frequency_type irs::cpu_traits_t::frequency()
 {
   return m_frequency;
 }
 
 #ifdef IRS_STM32_F2_F4_F7
-irs::cpu_traits_t::frequency_type
-irs::cpu_traits_t::periphery_frequency_first()
+irs::cpu_traits_t::frequency_type irs::cpu_traits_t::periphery_frequency_first()
 {
   return m_periphery_frequency_first;
 }
 
-void irs::cpu_traits_t::periphery_frequency_first(
-  frequency_type a_frequency)
+void irs::cpu_traits_t::periphery_frequency_first(frequency_type a_frequency)
 {
   m_periphery_frequency_first = a_frequency;
 }
 
-irs::cpu_traits_t::frequency_type
-irs::cpu_traits_t::periphery_frequency_second()
+irs::cpu_traits_t::frequency_type irs::cpu_traits_t::periphery_frequency_second()
 {
   return m_periphery_frequency_second;
 }
 
-void irs::cpu_traits_t::periphery_frequency_second(
-  frequency_type a_frequency)
+void irs::cpu_traits_t::periphery_frequency_second(frequency_type a_frequency)
 {
   m_periphery_frequency_second = a_frequency;
 }
 
-irs::cpu_traits_t::frequency_type
-irs::cpu_traits_t::timer_frequency(size_t a_timer_base)
+irs::cpu_traits_t::frequency_type irs::cpu_traits_t::timer_frequency(size_t a_timer_base)
 {
-  if ((a_timer_base == IRS_TIM1_PWM1_BASE) ||
-    (a_timer_base == IRS_TIM8_PWM2_BASE) ||
-    (a_timer_base == IRS_TIM9_BASE) ||
-    (a_timer_base == IRS_TIM10_BASE) ||
-    (a_timer_base == IRS_TIM11_BASE)) {
-    return m_periphery_frequency_second*2;
+  if ((a_timer_base == IRS_TIM1_PWM1_BASE) || (a_timer_base == IRS_TIM8_PWM2_BASE) ||
+      (a_timer_base == IRS_TIM9_BASE) || (a_timer_base == IRS_TIM10_BASE) ||
+      (a_timer_base == IRS_TIM11_BASE))
+  {
+    return m_periphery_frequency_second * 2;
   } else {
-    return m_periphery_frequency_first*2;
+    return m_periphery_frequency_first * 2;
   }
 }
 
