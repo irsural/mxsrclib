95736fa71b4e93d97ac9c71965a8fe6238d885d9
KrasheninnikovMaxim <maxk008@gmail.com>
2025-08-14 18:42:49 +0500

    cherry-pick master git Класс simple_ftp_server_t вынесен в отдельный модуль и перенесен из calibrator в mxsrclib
    # Conflicts:
    #	CMakeLists.txt

---
+++ simple_ftp_server.h
@@ -0,0 +1,116 @@
+//! \file
+//! \ingroup network_in_out_group
+//! \brief Сервер протокола Simple FTP
+
+#ifndef SIMPLE_FTP_SERVERH
+#define SIMPLE_FTP_SERVERH
+
+#include <irsdefs.h>
+
+#include <hardflowg.h>
+#include <irscpp.h>
+#include <irsfs.h>
+
+#include <irsfinal.h>
+
+namespace irs {
+
+class simple_ftp_server_t
+{
+public:
+  explicit simple_ftp_server_t(hardflow_t* ap_hardflow, fs_t* ap_fs);
+  void show_status() const;
+  void tick();
+
+private:
+  enum {
+    channel = 2,
+  };
+
+  enum {
+    set_file_path_command = 1,
+    read_size_command = 2,
+    read_command = 3,
+    read_command_response = 4,
+    abort_command = 5, // Не используется. Может быть заменена.
+    error_command = 6,
+    read_version_command = 7,
+    file_path_response = 8,
+  };
+
+  enum status_t {
+    st_start_wait,
+    st_command_processing,
+    st_set_file_path_ack_wait,
+    st_set_file_path_processing,
+    st_show_file_path,
+    st_read_processing,
+    st_read_ack,
+    st_check_ack,
+
+    st_write_packet,
+    st_write_packet_wait,
+    st_read_header,
+    st_read_header_wait,
+    st_read_trash_data_wait,
+    st_read_data,
+    st_read_data_wait,
+  };
+
+#pragma pack(push, 1)
+
+  struct packet_t
+  {
+    typedef irs_u32 file_size_type;
+
+    enum {
+      data_max_size = 100,
+      checksum_size = 2,
+      ack_error_tag = 0xDE,
+    };
+
+    irs_u8 command;
+    irs_u8 packet_id;
+    irs_u8 data_size;
+    irs_u8 header_checksum;
+    irs_u8 data_checksum;
+    irs_u8 data[data_max_size];
+
+    packet_t():
+      command(0),
+      packet_id(0),
+      data_size(0),
+      header_checksum(0),
+      data_checksum(0),
+      data()
+    {
+    }
+  };
+
+#pragma pack(pop)
+
+  static void net_to_u32(irs_u8* ap_data, irs_u32* ap_u32);
+  static void u32_to_net(irs_u32 a_u32, irs_u8* ap_data);
+
+  irs_u32 m_server_version;
+  hardflow_t* mp_hardflow;
+  fs_t* mp_fs;
+  status_t m_status;
+  hardflow::fixed_flow_t m_fixed_flow;
+  packet_t m_packet;
+  irs_u8 m_packet_id;
+  irs_u8 m_packet_id_prev;
+  status_t m_oper_return;
+  bool m_is_read_time_inf;
+  vector<irs_u8> m_file;
+  size_t m_data_offset; // Указатель на текущую позицию в файле
+  irs_u8 m_check_ack_packet_id;
+  bool m_is_checksum_error;
+  timer_t m_trash_data_timer;
+  irs_string_t m_file_path;
+  irs_u32 m_file_path_size;
+};
+
+} // namespace irs
+
+#endif //SIMPLE_FTP_SERVERH
